2189e5132cc49b54887a3caa868e99f61e8b2c9a
samuel.rvg <samuel.rvg@gmail.com>
2020-07-31 20:18:08 -0400

    projeto inicial

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá Mundo</h1>'
+    lista = ['Mario', 'CS', 'GTA']
+    return render_template('lista.html', titulo='Jogos do Samuel', subTitutlo='rvg', jogos=lista)
+
+#alterando o endereco e porta para iniciar o projeto
+#app.run(host='0.0.0.0', port=8080)
 
 app.run()
