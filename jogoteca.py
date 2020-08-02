5bc597706f9488590a92d8c801daf257cc2b8c1f
samuel.rvg <samuel.rvg@gmail.com>
2020-08-01 22:16:54 -0400

    criar novo jogo e habilitar modo debug

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -8,15 +8,29 @@ class Jogo:
         self.categoria = categoria
         self.console = console
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('GTA', 'Ação', 'PC')
-    jogo2 = Jogo('Grand Turismo', 'Corrida', 'PS4')
-    jogo3 = Jogo('PES2020', 'Futebol', 'PC')
-    lista = [jogo1, jogo2, jogo3]
+jogo1 = Jogo('GTA', 'Ação', 'PC')
+jogo2 = Jogo('Grand Turismo', 'Corrida', 'PS4')
+jogo3 = Jogo('PES2020', 'Futebol', 'PC')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='jogos do samuel', subTitutlo='rvg', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=["post",])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', subtituto='test', jogos=lista)
 
 #alterando o endereco e porta para iniciar o projeto
 #app.run(host='0.0.0.0', port=8080)
 
-app.run()
+app.run(debug=True)
